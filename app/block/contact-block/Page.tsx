3b0eeceae062aa09b4efe55a870dce0f8a722d05
Mohan Krishna A <mohankrishna.a757@gmail.com>
2025-04-18 16:20:17 +0530

    Add Contact component and ContactPage for user inquiries

---
+++ app/block/contact-block/Page.tsx
@@ -0,0 +1,17 @@
+import NavigationBar from "@/components/navigationbar/Navigation-Bar";
+import Container from "@/components/Container";
+import Contact from "./Contact";
+
+
+const ContactPage = () => {
+  return (
+    <>
+      <NavigationBar />
+      <Container>
+      <Contact/>
+      </Container>
+    </>
+  );
+};
+
+export default ContactPage;
