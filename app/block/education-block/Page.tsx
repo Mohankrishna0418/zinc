20aa5e9714d0e2e1b52d02c9ec1ba7ff6cf193e9
Mohan Krishna A <mohankrishna.a757@gmail.com>
2025-04-18 16:23:40 +0530

    Add EducationBlock and EducationCard components for educational details display

---
+++ app/block/education-block/Page.tsx
@@ -0,0 +1,18 @@
+import NavigationBar from "@/components/navigationbar/Navigation-Bar";
+import Container from "@/components/Container";
+import EducationBlock from "./Education";
+
+
+
+const ContactPage = () => {
+  return (
+    <>
+      <NavigationBar />
+      <Container>
+      <EducationBlock/>
+      </Container>
+    </>
+  );
+};
+
+export default ContactPage;
